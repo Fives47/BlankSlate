ce14e1e71d27ff792c23dd26428ba9e0b4ad48fe
ConorF-T <89919345+ConorF-T@users.noreply.github.com>
2024-12-08 17:16:22 -0500

    Added the floors from dungeon implementation and switched the tilesets for the darker floors

---
+++ UnderPressure/MapChange/Assets/mine..tsx
@@ -0,0 +1,234 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="mine" tilewidth="16" tileheight="16" tilecount="288" columns="16">
+ <image source="mine_dark" width="256" height="288"/>
+ <tile id="0">
+  <properties>
+   <property name="Type" value="Dirt"/>
+  </properties>
+ </tile>
+ <tile id="137">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="138">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="139">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="140">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="153">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="154">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="155">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="165">
+  <properties>
+   <property name="Type" value="Dirt"/>
+  </properties>
+ </tile>
+ <tile id="166">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="170">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="171">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="172">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="181">
+  <properties>
+   <property name="Type" value="Dirt"/>
+  </properties>
+ </tile>
+ <tile id="182">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="183">
+  <properties>
+   <property name="Type" value="Dirt"/>
+  </properties>
+ </tile>
+ <tile id="184">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="185">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="186">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="187">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="188">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="198">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="199">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="200">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="201">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="202">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="203">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="204">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="217">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="218">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="219">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="233">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="234">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="235">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="240">
+  <properties>
+   <property name="Type" value="Wood"/>
+  </properties>
+ </tile>
+ <tile id="241">
+  <properties>
+   <property name="Type" value="Wood"/>
+  </properties>
+ </tile>
+ <tile id="242">
+  <properties>
+   <property name="Type" value="Wood"/>
+  </properties>
+ </tile>
+ <tile id="256">
+  <properties>
+   <property name="Type" value="Wood"/>
+  </properties>
+ </tile>
+ <tile id="257">
+  <properties>
+   <property name="Type" value="Wood"/>
+  </properties>
+ </tile>
+ <tile id="258">
+  <properties>
+   <property name="Type" value="Wood"/>
+  </properties>
+ </tile>
+ <tile id="272">
+  <properties>
+   <property name="Type" value="Wood"/>
+  </properties>
+ </tile>
+ <tile id="273">
+  <properties>
+   <property name="Type" value="Wood"/>
+  </properties>
+ </tile>
+ <tile id="274">
+  <properties>
+   <property name="Type" value="Wood"/>
+  </properties>
+ </tile>
+</tileset>
